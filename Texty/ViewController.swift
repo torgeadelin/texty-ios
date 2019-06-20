55cf26928cafac6dcdb2076bf1812832fcba1e43
Catalin Torge <torgeadelin@gmail.com>
2019-06-20 12:44:11 +0100

    Initial Commit

---
+++ Texty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Texty
+//
+//  Created by Torge Adelin on 20/06/2019.
+//  Copyright © 2019 Torge Adelin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
